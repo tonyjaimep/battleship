f013bcd510c8643d23960bdffe350539dc4956a1
Antonio Jaime <contacto@antoniojai.me>
2020-01-30 08:40:31 -0600

    Add boards migration

---
+++ battleship-online/database/migrations/2020_01_30_143428_create_boards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('match_id');
+            $table->unsignedBigIngeger('user_id');
+            $table->unsignedInteger('size')->default(10);
+
+            $table->foreign('match_id')->references('id')->on('matches')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
