985da94d057cd0000411dd7dc3adac374f872b0f
Antonio Jaime <contacto@antoniojai.me>
2020-01-30 08:46:44 -0600

    Add pieces migration

---
+++ battleship-online/database/migrations/2020_01_30_144153_create_pieces_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePiecesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pieces', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('type', 1);
+            $table->char('orientation', 1);
+            $table->unsignedInteger('x');
+            $table->unsignedInteger('y');
+            $table->unsignedBigInteger('board_id');
+
+            $table->foreign('board_id')->references('id')->on('boards')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pieces');
+    }
+}
