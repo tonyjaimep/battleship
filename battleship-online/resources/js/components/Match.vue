5233615dc6cab6019cf812077eb6ee2f131dd72b
tonyjaimep <tonyjaimep@gmail.com>
2020-02-03 22:25:40 -0600

    Add board components

---
+++ battleship-online/resources/js/components/Match.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <div class="col-xl-8 col-lg-9">
+            <board :ships="ships" :attacks="attacks.received" :size="board.size"></board>
+        </div>
+        <div class="col-xl-4 col-lg-3">
+            <board :attacks="attacks.sent" :size="board.size"></board>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            ships: [],
+            attacks: {
+                sent: [],
+                received: [],
+            },
+            board: {
+                size: 10
+            }
+        }
+    }
+}
+</script>
