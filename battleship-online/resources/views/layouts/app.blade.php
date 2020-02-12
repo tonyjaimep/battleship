a080f1bd179bf8c72dee20bc97eaf78a3f5a2491
tonyjaimep <tonyjaimep@gmail.com>
2020-02-12 14:18:47 -0600

    Add CSRF token to every page

--- battleship-online/resources/views/layouts/app.blade.php
+++ battleship-online/resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - Battleship Online</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('stylesheets')
