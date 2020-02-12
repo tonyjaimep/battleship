0cd946ba3b2acfd222e9b1c6edae674fbd209472
Antonio Jaime <contacto@antoniojai.me>
2020-02-11 22:22:37 -0600

    Add match routes

--- battleship-online/routes/api.php
+++ battleship-online/routes/api.php
@@ -1,4 +1,5 @@
 <?php
 
-Route::put('/match/{match_id}/piece', 'PieceController@place');
+Route::post('/match/{match_id}/piece', 'PieceController@place');
 Route::put('/match/{match_id}/attack', 'AttackController@save');
+Route::get('/match/{match_id}/state', 'MatchController@getState');
