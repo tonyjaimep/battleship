a782da84e7796f90c001035c808d31f7dc92742c
tonyjaimep <tonyjaimep@gmail.com>
2020-02-12 23:34:10 -0600

    Add match won observer

---
+++ battleship-online/app/Events/MatchWon.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+use App\Match;
+
+class MatchWon implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $match;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Match $match)
+    {
+        $this->match = $match;
+        $this->match->boardA->destroy();
+        $this->match->boardB->destroy();
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('match.' . $this->match->id);
+    }
+}
--- battleship-online/app/Observers/MatchObserver.php
+++ battleship-online/app/Observers/MatchObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Match;
 
 use App\Events\MatchStateUpdated;
+use App\Events\MatchWon;
 
 class MatchObserver
 {
@@ -12,6 +13,9 @@ class MatchObserver
     {
         if ($match->getOriginal('state') != $match->state) {
             broadcast(new MatchStateUpdated($match));
+        }
+        if ($match->winner_id && $match->getOriginal('winner_id') == NULL) {
+            broadcast(new MatchWon($match));
         }
     }
 }
