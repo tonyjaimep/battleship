4298de521e24896ff5b9a3568672323884bc32c5
tonyjaimep <tonyjaimep@gmail.com>
2020-02-12 21:34:04 -0600

    Attack does not need to know user

--- battleship-online/database/migrations/2020_01_30_150520_create_attacks_table.php
+++ battleship-online/database/migrations/2020_01_30_150520_create_attacks_table.php
@@ -16,13 +16,11 @@ class CreateAttacksTable extends Migration
         Schema::create('attacks', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->dateTime('created_at');
-            $table->string('user_id');
             $table->unsignedBigInteger('target_board_id');
             $table->unsignedInteger('target_x');
             $table->unsignedInteger('target_y');
             $table->boolean('hit')->nullable();
 
-            $table->foreign('user_id')->references('id')->on('sessions')->onDelete('cascade');
             $table->foreign('target_board_id')->references('id')->on('boards')->onDelete('cascade');
         });
     }
